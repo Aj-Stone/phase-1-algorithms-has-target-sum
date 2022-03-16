6e3a89e1d4e522cc8e06e3b40012d40930de592e
Alex Atsitty <atsitty.alex1128@gmail.com>
2022-03-16 13:21:00 -0600

    commit tree 14151e621b22b045d260349256b703446f06dd7b

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  for (let i=0; i < array.length; i++){
+    for (let j= i+1; j < array.length; j++){
+      if (array[j] + array[i] === target){
+        return array[j] + array[i] === target
+      }
+    }
+  }
 }
 
 /* 
@@ -8,6 +15,9 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  - takes in array and target 
+  - itterates through the array
+  - checks to see if any 2 numbers from the array add up to the target value 
 */
 
 /*
@@ -29,6 +39,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log('')
+
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([1,2,12,24],25))
+
+  console.log('')
+
+  console.log('Expected: false')
+  console.log('=>', hasTargetSum([4], 4))
 }
 
 module.exports = hasTargetSum;
